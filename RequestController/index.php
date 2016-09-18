8078deccf3df72acefc350f2a707940488a573ed
ilher <pedro_reis14@hotmail.com>
2016-09-18 00:00:45 -0300

    Ocorreu um erro na hora que eu enviei pois enviou como outro usuario

--- RequestController/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "request_controller.php";
-require "request.php";
-$rc = new RequestController();
-var_dump(is_dir(substr($_SERVER['REQUEST_URI'], 1, -1)));
-var_dump($rc->create_request($_SERVER));
