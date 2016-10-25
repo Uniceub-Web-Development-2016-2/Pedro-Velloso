9649e2c7a414a75ffbd9a34b30825fbdd7d9ff49
root <dramakaukau@gmail.com>
2016-10-25 20:04:27 -0200

    Inserindo remotamente no banco do Caio

---
+++ client_aula8/insert.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<body>
+ 
+<form action="requestinsert.php" method="post">
+  <input type="text" name="first_name" placeholder="Primeiro nome">
+  <input type="text" name="last_name" placeholder="Ultimo nome">
+  <input type="email" name="email" placeholder="E-mail">
+  <input type="password" name="password" placeholder="Senha">
+  <input type="submit" value="Submit">
+
+
+</form> 
+
+</body>
+
+
+
+</html>
+
