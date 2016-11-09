602dec23e0ef1ad4863d21123e4df1c5d4fb8130
ilher <pedro_reis14@hotmail.com>
2016-11-09 04:34:08 -1100

    Inicio do desenvolvimento de tela HTML

---
+++ clientserv/request.php
@@ -0,0 +1,36 @@
+<?php
+include ("./httpful.phar");
+
+function request($resource, $operation, $method, array $qArray = NULL){
+
+	//Criação da URI com parametros dinamicos
+	$uri = "localhost/restserv/{$resource}/{$operation}".arrayToQueryString($qArray);
+	if($method != "GET"){
+		//Encode da body caso $_POST ou $_GET (method="post" ou method="get" no form)
+		$json = (count($_POST) > 0) ? json_encode($_POST) : json_encode($_GET);
+		//Request para POST, PUT e DELETE
+		$response = \Httpful\Request::{$method}($uri)->sendsJson()->body($json)->send();
+	} else {
+		//Request para GET
+		$response = \Httpful\Request::get($uri)->send();
+	}
+
+	//Resposta da Request
+
+	echo $response->body;
+}
+
+
+function arrayToQueryString(array $a = NULL){
+
+	$qS = NULL;
+	if(!is_null($a)){
+		$qS = "?";
+		foreach($a as $key => $value) {
+			$qS .= "{$key}={$value}&";	
+		}		
+	}	
+
+	return substr($qS, 0, -1);
+
+}
